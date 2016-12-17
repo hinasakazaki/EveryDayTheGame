9171fdb67df66accbc02b13eb7969cb78fe924d9
Hina Sakazaki <hinasakazaki@gmail.com>
2016-12-17 11:40:55 -0800

    first commit of scripts and stand-in assets

---
+++ BalloonGame/Assets/SideScrollingScript.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SideScrollingScript : MonoBehaviour {
+
+	private Vector3 startPosition;
+	public float scrollSpeed;
+    public float tileSizeX;
+
+	// Use this for initialization
+	void Start () {
+		startPosition = transform.position;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		float newPosition = Mathf.Repeat(Time.time * scrollSpeed, tileSizeX);
+        transform.position = startPosition + Vector3.left * newPosition;
+	}
+}
---
+++ BalloonGame/Assets/scripts/MusicScript.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MusicScript : MonoBehaviour {
+
+    public GameObject bgAudio;
+    AudioSource audioSrc;
+    float[] samples = new float[256];
+	// Use this for initialization
+	void Start () {
+        audioSrc = bgAudio.GetComponent<AudioSource>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        audioSrc.GetOutputData(samples, 0);
+        float max = 0.0f;
+        float sum = 0.0f;
+        for (int i = 0; i < 256; i++)
+        {
+            sum += samples[i] * samples[i];
+        }
+        float rms = Mathf.Sqrt(sum / 1024);
+        //GetComponent<GUIText>().text = rms.ToString();
+       // Debug.Log(rms.ToString());
+    }
+}
