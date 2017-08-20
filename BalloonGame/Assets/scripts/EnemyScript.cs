797666d2bd5bf3f755167b4293b6856c389884da
Hina Sakazaki <hinasakazaki@gmail.com>
2017-08-20 08:26:11 -0700

    this is where we start branching for projectile vs. radar

--- BalloonGame/Assets/scripts/EnemyScript.cs
+++ BalloonGame/Assets/scripts/EnemyScript.cs
@@ -33,7 +33,7 @@ public class EnemyScript : MonoBehaviour {
 
     // Update is called once per frame
     void Update() {
-      //  doCollisionWithRadar();
+        doCollisionWithRadar();
 	}
 
     private void doCollisionWithRadar()
